8acbfc79ae8d31592db391a790262b794e51ecf9
Eric Churchill <eric.churchill2@gmail.com>
2017-02-11 15:30:33 -0800

    fix linting errors

--- client/highlight.js
+++ client/highlight.js
@@ -3,10 +3,10 @@ const elements = document.getElementsByTagName('*');
 // TODO: this value to be set
 let selection;
 
-for (let i = 0; i < elements.length; i++) {
+for (let i = 0; i < elements.length; i += 1) {
   const element = elements[i];
 
-  for (let j = 0; j < element.childNodes.length; j++) {
+  for (let j = 0; j < element.childNodes.length; j += 1) {
     const node = element.childNodes[j];
 
     if (node.nodeType === 3) {
@@ -14,7 +14,7 @@ for (let i = 0; i < elements.length; i++) {
       if (text.indexOf(selection) >= 0) {
         const dataArr = text.split(selection);
 
-        element.innerHTML = dataArr[0] + "<mark>" + selection + "</mark>" + dataArr[1];
+        element.innerHTML = `${dataArr[0]} <mark>${selection}</mark> ${dataArr[1]}`;
         i = elements.length;
         j = element.childNodes.length;
       }
