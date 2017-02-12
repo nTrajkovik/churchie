12d36d804b93f9cfe89ab025544507c28a86e73d
elliottabirch <elliottabirch@gmail.com>
2017-02-12 08:25:18 -0800

    implement storage of username on chrome

--- client/src/toolbar-menu-bundle/components/App/index.jsx
+++ client/src/toolbar-menu-bundle/components/App/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import Signin from '../../components/Signin';
 
-function App() {
-  return <div>Hello world!</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+    };
+    this.setStorageSync = this.setStorageSync.bind(this);
+  }
+
+
+  componentWillMount() {
+    chrome.storage.sync.get('username', a => this.setUsername(a));
+  }
+
+  setUsername(name) {
+    this.setState({
+      username: name,
+    }, () => console.log('username set'));
+  }
+
+  setStorageSync(name) {
+    chrome.storage.sync.set({ username: name }, () => console.log('username set on sync'));
+    this.setUsername(name);
+  }
+
+  render() {
+    return this.state.username ? <div>Hello world!</div> : <Signin setStorageSync={this.setStorageSync} />;
+  }
+
+
 }
 
 export default App;
