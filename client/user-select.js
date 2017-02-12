9e385578756633d14aaaf685644847bbcd5cd7e2
ashermanwmf <shermanandrew50@gmail.com>
2017-02-11 21:23:57 -0800

    wrapped user-select in promise to invoke react render on event resolve

--- client/user-select.js
+++ client/user-select.js
@@ -1,18 +1,21 @@
-document.body.addEventListener('mouseup', (e) => {
-  let text = '';
-  if (window.getSelection) {
-    text = window.getSelection().toString();
-  } else if (document.selection && document.selection.type !== 'Control') {
-    text = document.selection.createRange().text;
-  }
-  if (text) {
-    const nodes = e.target.childNodes;
-    nodes.forEach((node) => {
-      if (node.nodeType === 3) {
-        const innerText = node.nodeValue;
-        const dataArr = innerText.split(text);
-        e.target.innerHTML = `${dataArr[0]}<mark>${text}</mark>${dataArr[1]}`;
-      }
-    });
-  }
+export default new Promise((resolve, reject) =>{
+  document.body.addEventListener('mouseup', (e) => {
+    let text = '';
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type !== 'Control') {
+      text = document.selection.createRange().text;
+    }
+    if (text) {
+      const nodes = e.target.childNodes;
+      nodes.forEach((node) => {
+        if (node.nodeType === 3) {
+          const innerText = node.nodeValue;
+          const dataArr = innerText.split(text);
+          e.target.innerHTML = `${dataArr[0]}<mark>${text}</mark>${dataArr[1]}`;
+        }
+      });
+      resolve();
+    }
+  });
 });
