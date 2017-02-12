d9989256212ea87b7afef63d0694a2375659c784
Eric Churchill <eric.churchill2@gmail.com>
2017-02-11 20:14:25 -0800

    Fix Annotation syntax
    Update functionality to make call to PubNub endpoint. TODO: enter url

--- client/views/page-content/Annotation/index.jsx
+++ client/views/page-content/Annotation/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import Comments from '../../../components/App/Comments/index';
 
 class Annotation extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      comments: [],
+    };
+  }
+
+  componentWillMount() {
+    axios.get(/* TODO: get url */)
+      .then((res) => {
+        this.setState({ comments: res });
+      });
   }
 
   render() {
     return (
-      <Comments />
+      <Comments props={this.state} />
     );
   }
 }
